2a4debf82ba62ef118b154faed8ca4f1fb9501d5
6135512027 <carelovejaja@hotmail.com>
2020-09-17 11:01:50 +0700

    bg

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Weather from './Weather'
+import Weather from './components/Weather'
 
 export default function App() {
 
   return (
-    <View style={styles.container} >
+    <View>
        <Weather zipCode="83120" /> 
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    borderWidth: 5,
-    borderColor: 'red',    
-  }
-})
