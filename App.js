fa4377874595e8254dc61d347713b782852fd6d3
6135512027 <carelovejaja@hotmail.com>
2020-09-17 10:42:58 +0700

    BLOCK

--- App.js
+++ App.js
@@ -4,11 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import Weather from './Weather'
 
 export default function App() {
-  
+
   return (
-    <View>
-      <Weather zipCode="83120"/>
+    <View style={styles.container} >
+       <Weather zipCode="83120" /> 
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    borderWidth: 5,
+    borderColor: 'red',    
+  }
+})
--- Weather.js
+++ Weather.js
@@ -1,13 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, PickerIOSComponent } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function Weather(props) {
 
   return (
-    <View>
-      <Text > Weather {props.zipCode} </Text>
+    <View  style={styles.container} >
+      <Text style={{ fontSize: 20 }}> Weather  {props.zipCode} </Text>
+      <View style={{width: 20, height: 20, backgroundColor: 'red'}}></View>     
+      <View style={{width: 20, height: 20, backgroundColor: 'blue'}}></View>     
+      <View style={{width: 20, height: 20, backgroundColor: 'green'}}></View>           
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',   
+    alignItems: 'center' ,
+    borderWidth: 5,
+    borderColor: 'red',    
+  }
+})
